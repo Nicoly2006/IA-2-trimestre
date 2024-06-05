600d99a0d504afe871ba3ac55ee20ee07d2ab8fe
Nicoly2006 <126686526+Nicoly2006@users.noreply.github.com>
2024-06-05 09:45:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const caixaPrincipal = document.querySelector(".caixa-pricipal")
+const caixaPerguntas = document.querySelector(".caixa-perguntas")
+const caixaAlternativas = document.querySelector(".caixa-alternativas")
+const caixaResultado = document.querySelector(".caixa-reultado")
+const textoResultado = document.querySelector(".texto-resultado")
+
+const perguntas = [
+    {
+        enunciado: "pergunta1"
+    alternativas: ["alternativa1", "alternativa2"]
+    },
+
+    {
+        enunciado: "pergunta2"
+    alternativas: ["alternativa1", "alternativa2"]
+    },
+
+]
